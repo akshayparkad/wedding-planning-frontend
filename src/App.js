527e87c28ec3ebb743808f18fd861344a98c2f7c
Sagar Kamble <sagarkamble2431992@gmail.com>
2023-01-03 00:53:36 +0530

    sagar 2nd commmit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./components/home/Home.css"
+import "./components/home/footer/Footer.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Vendor from "./components/vendors/Vendor";
 import Home from "./components/home/Home";
-
+import { Header } from "./components/home/header/Header";
+import { Footer } from "./components/home/footer/Footer";
 function App() {
   return (
     <BrowserRouter>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/vendor" element={<Vendor />}></Route>
       </Routes>
+      <Footer></Footer>
     </BrowserRouter>
   );
 }
