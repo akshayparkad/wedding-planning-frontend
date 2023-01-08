e4b69140a7b1d3843b2b0f181e1c8ccfe5984e69
krsonani <krunalsonanir@gmail.com>
2023-01-08 21:48:19 +0530

    order

--- src/components/Service-detail/ServiceDetail.js
+++ src/components/Service-detail/ServiceDetail.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/js/bootstrap.bundle.js";
 import "./ServiceDetail.css";
 import { useNavigate, useParams } from "react-router-dom";
 import { getSpacificService } from "../../Service/WedService";
+import { addOrder } from "../../Service/User";
 
 export default function ServiceDetail() {
   const params = useParams();
@@ -16,15 +17,21 @@ export default function ServiceDetail() {
   useEffect(() => {
     getWedServicBySid();
   }, [params]);
+
+  const addorder = async () => {
+    const order = {
+      vendorname: service.vendorname,
+      category: service.category,
+      price: service.price1,
+      uid: service.uid,
+    };
+    const responce = await addOrder(order);
+  };
   return (
     <div className="container">
       <div className="row mt-3">
         <div className="col-7 ">
-          <img
-            src=
-            { service.img1}
-            className="img-fluid img-ak-1"
-          ></img>
+          <img src={service.img1} className="img-fluid img-ak-1"></img>
         </div>
         <div className="col-5 px-5 ">
           <div className="boxes-ak-1">
@@ -32,16 +39,18 @@ export default function ServiceDetail() {
               <h5 className="text-secondary">Per Day Price Estimate</h5>
             </div>
             <div className="border-ak-1 ">
-              <span className="color-ak-1 fs-4">{ service.price1} per day</span>
-              <span>{ service.purpose1}</span>
+              <span className="color-ak-1 fs-4">{service.price1} per day</span>
+              <span>{service.purpose1}</span>
             </div>
             <div>
-              <span className="color-ak-1 fs-4">{ service.price2}per day</span>
-              <span>{ service.purpose2}</span>
+              <span className="color-ak-1 fs-4">{service.price2}per day</span>
+              <span>{service.purpose2}</span>
             </div>
           </div>
           <div className="detail-ak-1 my-4 ">
-            <button className="btn-d-ak-2 mt-4">Order</button>
+            <button className="btn-d-ak-2 mt-4" onClick={addorder}>
+              Order
+            </button>
             <div className="my-2">
               <spna className="px-5">High Demand </spna>
               <span className="px-5 ">7 enquiries last week </span>
@@ -51,10 +60,10 @@ export default function ServiceDetail() {
       </div>
       <div className="row ">
         <div className="col-7 boxes-ak-2 ">
-          <div className="fs-3 py-1 color-ak-1">{ service.username}</div>
-          <div className="fs-5 py-1 text-muted">{ service.location}</div>
+          <div className="fs-3 py-1 color-ak-1">{service.vendorname}</div>
+          <div className="fs-5 py-1 text-muted">{service.location}</div>
           <div className="fs-5 py-1 text-muted">
-            Event Handle Exp:-{ service.exp} years{" "}
+            Event Handle Exp:-{service.exp} years{" "}
           </div>
         </div>
       </div>
@@ -68,33 +77,20 @@ export default function ServiceDetail() {
       <div className="row my-4  ">
         <hr></hr>
         <div className="col-4 px-1">
-          <img
-            src={ service.img2}
-            className=" img-ak-d-1"
-          ></img>
+          <img src={service.img2} className=" img-ak-d-1"></img>
         </div>
         <div className="col-4 px-2">
-          <img
-           src={ service.img3}
-            className=" img-ak-d-1"
-          ></img>
+          <img src={service.img3} className=" img-ak-d-1"></img>
         </div>
         <div className="col-4 px-2">
-          <img
-            src={ service.img4}
-            className=" img-ak-d-1"
-          ></img>
+          <img src={service.img4} className=" img-ak-d-1"></img>
         </div>
       </div>
       <div>
         <hr></hr>
-        <div className=" h5 text-secondary ">
-          { service.description}
-        </div>
+        <div className=" h5 text-secondary ">{service.description}</div>
         <hr></hr>
-        <div className=" text-muted">
-          { service.content}
-        </div>
+        <div className=" text-muted">{service.content}</div>
       </div>
       <hr></hr>
     </div>
